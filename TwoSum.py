3727f6ca2ea2c9ae012140fcffba1155244c217b
Anurag Bhandary <anuragb2901@gmail.com>
2024-05-27 13:20:37 -0500

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,6 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ref = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in ref:
+                return [i, ref[complement]]
+            ref[nums[i]] = i
+
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return ([i,j])
+'''
