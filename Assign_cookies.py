c1abeb9b01e7c4434c4a3d7c96f6caa576a017c4
Anurag Bhandary <anuragb2901@gmail.com>
2024-07-18 13:17:01 -0500

    Create Assign_cookies.py
    NEED A BETTER SOLUTION USING TWO POINTERS😞

---
+++ Assign_cookies.py
@@ -0,0 +1,16 @@
+# NEED A BETTER SOLUTION
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        s.sort(reverse=True)
+        g.sort()
+        l = len(g)
+        
+
+        for i in range(l-1, -1, -1):
+            for j in range(len(s)):
+                if s[j] >= g[i]:
+                    s.pop(j)
+                    g.pop(i)
+                    break
+
+        return l - len(g)
